7e537d49e139dac2965fc38ad805f5e85c9d2c03
Francesco Dalmonte <dalmonte.fr@gmail.com>
2023-05-02 12:09:23 +0200

    adeed docs in image_processing.py

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -4,24 +4,45 @@ import numpy as np
 
 def randomFlip(crop):
     """
+    Performs random horizontal/vertical flip of an input image (with p = 0.5).
     """
     if np.random.rand() > .5 : crop = crop[:, ::-1]
     if np.random.rand() > .5 : crop = crop[::-1, :]
+
     return crop
     
 
 
 def randomShift(x, y, max_shift = 100):
     """
+    Performs a random shift of an input point [x,y] (max displacement = max_shift).
     """
     x += np.random.randint(-max_shift, max_shift)
     y += np.random.randint(-max_shift, max_shift)
+    
     return x, y
 
 
-def cropImage(image, centers, size = 224,
-              rand_shift = False, rand_flip = False):
+def cropImage(image: np.array,
+              centers: np.array,
+              size: int = 224,
+              rand_shift: bool = False,
+              rand_flip: bool = False) -> np.array:
     """
+    Returns a set of fixed-size square crops of an input image.
+
+    Parameters
+    ----------
+    image: image to be cropped.
+    centers: set of centers coordinates of the crops.
+    size: side length of the crop (pixels. Default = 224)
+    rand_shift: perform random shift of the centers (default = False).
+    rand_flip: perform random flip of the crops (default = False).
+
+    Returns
+    ----------
+    crops_set: set of crops.
+
     """
     crops_set = []
 
