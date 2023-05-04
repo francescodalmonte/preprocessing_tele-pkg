483dd49b0fc0cab762a4dacaf5659a8d22b236b3
Francesco Dalmonte <dalmonte.fr@gmail.com>
2023-05-04 15:47:02 +0200

    modified dataset creation script

--- create_cropsDataset.py
+++ create_cropsDataset.py
@@ -1,13 +1,29 @@
 import numpy as np
+import os
 import time
 
 from utils.multiChannelImage import multiChannelImage
-
+from utils.IO import listRawDir
 np.random.seed(999)
+
+ROOT = os.path.abspath("C:/Users/Francesco/Pictures/tele/raw")
 
 if __name__ == "__main__":
     start = time.time()
 
+    rawNames = listRawDir(ROOT)
+    
+    for name in rawNames:
+        object = multiChannelImage(name, ROOT)
+
+        anomalousCrops, anomalousCenters = object.fetch_anomalousCrops(scale = 1.,
+                                                                       rand_flip = True,
+                                                                       rand_shift = True)
+
+        goodCrops, goodCenters = object.fetch_goodCrops(scale = 1., N = 1000)        
+        
+        print(f"{name} - {len(anomalousCrops)}/{len(goodCrops)}")
+
     ##### code ####
 
-    print(f"Finish! Elapsed time: {(time.time()-start):2f} s")
+    print(f"Elapsed time: {(time.time()-start):2f} s")
